af209b9375d04c8d103d36766ffc1df93c83d15a
lemonmojo <office@lemonmojo.com>
2018-05-05 10:27:43 +0200

    Initial commit

---
+++ ELib/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ELib
+{
+    public class Person
+    {
+		public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+
+        public string FullName
+		{
+			get {
+				return FirstName + " " + LastName;               
+			}
+		}
+
+        public Person() { }
+
+		public Person(string firstName, string lastName)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+		}
+
+        public void ThrowException()
+        {
+            ThrowException("A nice Exception Message");
+        }
+
+        public void ThrowException(string message)
+        {
+            throw new Exception(message);
+        }
+
+        public void DoIntesiveWork()
+        {
+            int i = 0;
+
+            do {
+                Print("DoIntesiveWork " + i);
+
+                i++;
+            } while (true);
+        }
+
+        public void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Embeddinator18Tester/Embeddinator18Tester/AppDelegate.m
@@ -0,0 +1,51 @@
+#import "AppDelegate.h"
+
+#import <ELib/ELib.h>
+
+@interface AppDelegate ()
+@property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *textFieldFullName;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    ELib_Person* person = self.johnDoe;
+    
+    self.textFieldFullName.stringValue = person.fullName;
+}
+
+- (ELib_Person*)johnDoe
+{
+    ELib_Person* person = [[ELib_Person alloc] initWithFirstName:@"John" lastName:@"Doe"];
+    
+    return person;
+}
+
+- (IBAction)buttonThrowException_action:(id)sender
+{
+    ELib_Person* person = self.johnDoe;
+    
+    @try {
+        [person throwException];
+    } @catch (NSException* ex) {
+        NSString* message = ex.reason;
+        NSString* typeFullName = ex.name;
+        NSString* stackTrace = ex.callStackSymbols.description;
+        
+        NSLog(@"Ex Message: %@; Type Name: %@", message, typeFullName);
+        NSLog(@"Ex StackTrace: %@", stackTrace);
+    }
+}
+
+- (IBAction)buttonDoIntensiveWork_action:(id)sender
+{
+    ELib_Person* person = self.johnDoe;
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        [person doIntesiveWork];
+    });
+}
+
+@end
