c1651ae76d1eaa2b1741e66b9dca7c6a16ea3e34
lemonmojo <office@lemonmojo.com>
2018-05-29 21:20:21 +0200

    Event handling tests

---
+++ ELib/NativeEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Foundation;
+
+namespace ELib
+{
+    public class NativeEvent
+    {
+		public const string KEY_EVENTARGS = "EventArgs";
+
+		public static void Invoke(string notificationName, NativeEventArgs eventArgs)
+        {
+            NSNotification notification = CreateNotification(notificationName, eventArgs);
+
+            NSNotificationCenter.DefaultCenter.PostNotification(notification);
+        }
+
+		private static NSNotification CreateNotification(string notificationName, NativeEventArgs eventArgs)
+        {
+			long eventArgsPtr = eventArgs != null ? eventArgs.NativeHandle : 0;
+
+            NSDictionary<NSObject, NSObject> userInfo = NSDictionary<NSObject, NSObject>.FromObjectsAndKeys(
+                new [] { NSNumber.FromInt64(eventArgsPtr) }, new [] { (NSString)KEY_EVENTARGS }, 1
+            );
+
+            NSNotification notification = NSNotification.FromName(notificationName, null, userInfo);
+
+            return notification;
+        }
+    }
+}
--- Embeddinator18Tester/Embeddinator18Tester/AppDelegate.m
+++ Embeddinator18Tester/Embeddinator18Tester/AppDelegate.m
@@ -7,7 +7,9 @@
 @property (weak) IBOutlet NSTextField *textFieldFullName;
 @end
 
-@implementation AppDelegate
+@implementation AppDelegate {
+    BOOL m_eventObserverRegistered;
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
@@ -47,6 +49,43 @@
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
         [person doIntesiveWork];
     });
+}
+
+- (IBAction)buttonEventTest_action:(id)sender
+{
+    [self registerEventObservers];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        ELib_NativeEventsTest* eventTest = [[ELib_NativeEventsTest alloc] init];
+        
+        [eventTest raiseMessageReceived];
+    });
+}
+
+- (void)registerEventObservers
+{
+    if (m_eventObserverRegistered) {
+        return;
+    }
+    
+    [NSNotificationCenter.defaultCenter addObserver:self
+                                           selector:@selector(messageReceived:)
+                                               name:ELib_NativeEventsTest.nOTIFICATIONNAME_MESSAGERECEIVED
+                                             object:nil];
+    
+    m_eventObserverRegistered = YES;
+}
+
+- (void)messageReceived:(NSNotification*)notification
+{
+    NSInteger eventArgsPtr = ((NSNumber*)notification.userInfo[ELib_NativeEvent.kEY_EVENTARGS]).intValue;
+    ELib_MessageReceivedEventArgs *eventArgs = [ELib_MessageReceivedEventArgs fromPtrPtr:eventArgsPtr];
+    
+    NSLog(@"Message received in native: %@", eventArgs.message);
+    
+    eventArgs.message = @"Message was set in native code";
+    
+    [eventArgs dispose];
 }
 
 @end
